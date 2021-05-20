61bf966ecbb4d111bcbaba7dc34a057b5d578a5c
leolab1337 <leohannolainen999@gmail.com>
2021-05-20 11:27:20 +0300

    create 8Button

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    private Button resetBtn, deleteBtn, num9Btn;
+    private Button resetBtn, deleteBtn, num9Btn,num8Btn ;
     private TextView resultTV;
 
     @Override
@@ -21,6 +21,7 @@ public class MainActivity extends AppCompatActivity {
         resetBtn = findViewById(R.id.resetBtn);
         deleteBtn = findViewById(R.id.deleteBtn);
         num9Btn = findViewById(R.id.num9Btn);
+        num8Btn = findViewById(R.id.num8Btn);
 
         //Events
         resetBtn.setOnClickListener(v -> resultTV.setText("") );
@@ -36,6 +37,11 @@ public class MainActivity extends AppCompatActivity {
         num9Btn.setOnClickListener(v -> {
             String userInput = resultTV.getText().toString();
             userInput += "9";
+            resultTV.setText(userInput);
+        });
+        num8Btn.setOnClickListener(v -> {
+            String userInput = resultTV.getText().toString();
+            userInput += "8";
             resultTV.setText(userInput);
         });
     }
